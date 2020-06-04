efc924614a13e375b086aaa1fb694876f45594d3
Alexandra Espichan <aespichanlinares@gmail.com>
2020-06-03 21:04:50 -0500

    CRUD

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,5 +3,8 @@ class Page < ApplicationRecord
   has_many :sections
   has_and_belongs_to_many :admin_users
 
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
   scope :sorted, -> { order(:position) }
+  scope :newest_first, -> { order("created_at DESC") }
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -3,5 +3,8 @@ class Section < ApplicationRecord
   has_many :section_edits
   has_many :admin_users, through: :section_edits
   
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
   scope :sorted, -> { order(:position) }
+  scope :newest_first, -> { order("created_at DESC") }
 end
